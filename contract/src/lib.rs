268e49b4465d5e4b4a3238ad9782d605faa8b270
bhc8521 <bhc8521>
2022-05-25 17:46:47 +0800

    clear

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -3,12 +3,14 @@
 use std::convert::{TryFrom, TryInto};
 
 use bloom_filter::{Bloom, WrappedHash};
+use near_contract_standards::non_fungible_token::TokenId;
+use near_contract_standards::non_fungible_token::metadata::{NFTContractMetadata, TokenMetadata, NFT_METADATA_SPEC};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::{Base58CryptoHash, U128};
 use near_sdk::serde::{Serialize, Deserialize};
 use near_sdk::serde_json::{json, self};
 use near_sdk::{env, near_bindgen, AccountId, log, bs58, PanicOnDefault, Promise, BlockHeight};
-use near_sdk::collections::{LookupMap, UnorderedMap, Vector};
+use near_sdk::collections::{LookupMap, UnorderedMap, Vector, LazyOption};
 use utils::{check_args, verify, check_encrypt_args};
 use access::Access;
 
@@ -19,49 +21,14 @@ pub mod bloom_filter;
 pub mod post;
 pub mod access;
 
-
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Popula {
     owner_id: AccountId,
     public_key: String,
-    bloom_filter: Bloom
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(crate = "near_sdk::serde")]
-#[derive(Debug)]
-pub struct AccessInfo {
-    token_id: AccountId,
-    amount_to_access: U128
-}
-
-#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
-#[serde(crate = "near_sdk::serde")]
-#[derive(Debug)]
-pub struct EncryptInfo {
-    content: EncryptArgs,
-    access: String
-}
-
-#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
-#[serde(crate = "near_sdk::serde")]
-#[derive(Debug)]
-pub struct Args {
-    text: Option<String>,
-    imgs: Option<Vec<String>>,
-    video: Option<String>,
-    audio: Option<String>
-}
-
-#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
-#[serde(crate = "near_sdk::serde")]
-#[derive(Debug)]
-pub struct EncryptArgs {
-    text: Option<String>,
-    imgs: Option<String>,
-    video: Option<String>,
-    audio: Option<String>
+    bloom_filter: Bloom,
+    // metadata: LazyOption<NFTContractMetadata>,
+    // notice_metadata: UnorderedMap<TokenId, TokenMetadata>
 }
 
 #[near_bindgen]
@@ -69,11 +36,12 @@ impl Popula {
 
     #[init]
     pub fn new(public_key: String) -> Self {
-        Self {
+        let mut this = Self {
             owner_id: env::predecessor_account_id(),
             public_key: public_key,
-            bloom_filter: Bloom::new_for_fp_rate_with_seed(1000000, 0.1)
-        }
+            bloom_filter: Bloom::new_for_fp_rate_with_seed(1000000, 0.1),
+        };
+        this
     }
 }
 
--- contract/src/post.rs
+++ contract/src/post.rs
@@ -1,14 +1,17 @@
 use std::convert::TryInto;
 
+use near_contract_standards::non_fungible_token::events::NftMint;
+use near_sdk::CryptoHash;
+
 use crate::*;    
 
-#[derive(Serialize, Deserialize)]
-#[serde(crate = "near_sdk::serde")]
-#[derive(Debug)]
-pub struct EncryptInfo {
-    content: EncryptArgs,
-    access: Access
-}
+// #[derive(Serialize, Deserialize)]
+// #[serde(crate = "near_sdk::serde")]
+// #[derive(Debug)]
+// pub struct EncryptInfo {
+//     content: EncryptArgs,
+//     access: Access
+// }
 
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
@@ -17,31 +20,52 @@ pub struct Args {
     text: Option<String>,
     imgs: Option<Vec<String>>,
     video: Option<String>,
-    audio: Option<String>
+    audio: Option<String>,
+    options: Option<Vec<Options>>
 }
+
+// #[derive(Serialize, Deserialize)]
+// #[serde(crate = "near_sdk::serde")]
+// #[derive(Debug)]
+// pub struct EncryptArgs {
+//     text: Option<String>,
+//     imgs: Option<String>,
+//     video: Option<String>,
+//     audio: Option<String>,
+//     options: Option<Vec<Options>>
+// }
 
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 #[derive(Debug)]
-pub struct EncryptArgs {
-    text: Option<String>,
-    imgs: Option<String>,
-    video: Option<String>,
-    audio: Option<String>
+pub enum Options {
+    At(AccountId)
 }
 
 #[near_bindgen]
 impl Popula {
-    pub fn add_post(&mut self, args: String) -> String {
+    pub fn add_post(&mut self, args: String) -> Base58CryptoHash {
         let args_obj: Args = serde_json::from_str(&args).unwrap();
         check_args(args_obj.text, args_obj.imgs, args_obj.video, args_obj.audio);
         
         let args = args.clone() + &bs58::encode(env::random_seed()).into_string();
         let hash = env::sha256(&args.clone().into_bytes());
-        let hash_str = bs58::encode(hash.clone()).into_string();
-        let hash:[u8;32] = hash[..].try_into().unwrap();
+        let hash: CryptoHash = hash[..].try_into().unwrap();
         self.bloom_filter.set(&WrappedHash::from(hash));
-        hash_str
+        let hash = Base58CryptoHash::from(hash);
+        // match args_obj.options {
+        //     Some(options) => {
+        //         options.iter().for_each(|item| {
+        //             match item {
+        //                 Options::At(account_id) => {
+        //                     NftMint { owner_id: &account_id, token_ids: &["at"], memo: None }.emit();
+        //                 },
+        //             }
+        //         });
+        //     },
+        //     None => todo!()
+        // }
+        hash
     }
 
     // pub fn add_encrypt_post(&mut self, encrypt_info: String, sign: String) -> String {
@@ -73,7 +97,7 @@ impl Popula {
         assert!(self.bloom_filter.check(&WrappedHash::from(target_hash)), "content not found");
     }
 
-    pub fn add_comment(&mut self, args: String, target_hash: Base58CryptoHash) -> String {
+    pub fn add_comment(&mut self, args: String, target_hash: Base58CryptoHash) -> Base58CryptoHash {
         let target_hash = target_hash.try_to_vec().unwrap();
         let target_hash: [u8;32] = target_hash[..].try_into().unwrap();
         assert!(self.bloom_filter.check(&WrappedHash::from(target_hash)), "content not found");
@@ -83,10 +107,10 @@ impl Popula {
 
         let args = args.clone() + &env::block_height().to_string();
         let hash = env::sha256(&args.clone().into_bytes());
-        let hash_str = bs58::encode(hash.clone()).into_string();
-        let hash:[u8;32] = hash[..].try_into().unwrap();
+        let hash: CryptoHash = hash[..].try_into().unwrap();
         self.bloom_filter.set(&WrappedHash::from(hash));
-        hash_str
+        let hash = Base58CryptoHash::from(hash);
+        hash
     }
 
     // pub fn add_encrypt_comment(&mut self, encrypt_info: String, sign: Base58CryptoHash, target_hash: Base58CryptoHash) -> String {
